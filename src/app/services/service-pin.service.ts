75c64c9a1e2f617a5cec374660e1d3ef40765f8b
elkinjimenez <elkin.jiga@hotmail.com>
2020-09-17 15:26:02 -0500

    Prueba

--- src/app/services/service-pin.service.ts
+++ src/app/services/service-pin.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { PinGenerar , Pin, PinValidar } from '../Models/pin';
+import { PinGenerar, Pin, PinValidar } from '../Models/pin';
 import { HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
+    'Content-Type': 'application/json',
   })
 };
 
@@ -13,30 +13,27 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class ServicePinService {
-  
-  private respuesta : Pin  ; 
-  
+
+  private respuesta: Pin;
+
   private error: any;
-  constructor(private clientePin: HttpClient  ) {
-   }
-  
-   genPin( cuerpo: PinGenerar ){
+  constructor(private clientePin: HttpClient) {
+  }
 
+  genPin(cuerpo: PinGenerar) {
+    this.clientePin.post("http://100.126.19.74:7001/WsVoiceTraffic-web/webresources/VoiceTraffic/generaPin"
+      , cuerpo, httpOptions).subscribe();
+  }
 
-     this.clientePin.post("http://100.126.19.74:7001/WsVoiceTraffic-web/webresources/VoiceTraffic/generaPin"
-     ,cuerpo,httpOptions).subscribe();
-   }
+  valPin(cuerpo: PinValidar) {
+    this.clientePin.post("http://100.126.19.74:7001/WsVoiceTraffic-web/webresources/VoiceTraffic/validaPin"
+      , cuerpo, httpOptions).subscribe(data => {
+        this.respuesta = data as Pin;
+      }, error => { ; { console.log(error) }; });
+  }
 
-   valPin(cuerpo : PinValidar ){
-
-
-     this.clientePin.post("http://100.126.19.74:7001/WsVoiceTraffic-web/webresources/VoiceTraffic/validaPin"
-    ,cuerpo, httpOptions).subscribe(data => {this.respuesta = data as Pin;
-       }, error => { ;{console.log(error)}; }  );
-   }
-
-   getRta(){
-     return this.respuesta; 
-   }
+  getRta() {
+    return this.respuesta;
+  }
 
 }
